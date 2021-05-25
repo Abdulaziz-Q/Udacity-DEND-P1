47aff635ad764a38f4079d4bcfa5dd8df7bbf27e
Abdulaziz Alqumayzi <abdulaziz.alqumayzi@gmail.com>
2021-05-25 14:56:49 +0300

    updated codes

--- sql_queries.py
+++ sql_queries.py
@@ -9,7 +9,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
-songplay_id SERIAL  PRIMARY KEY, 
+songplay_id SERIAL PRIMARY KEY, 
 start_time time , 
 user_id int, 
 level varchar,
