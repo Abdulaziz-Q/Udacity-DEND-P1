847b2a53867a813c4deeab8243de9e3f9a9fe76b
Abdulaziz Alqumayzi <abdulaziz.alqumayzi@gmail.com>
2021-05-31 14:02:11 +0300

    Updated code

--- etl.py
+++ etl.py
@@ -7,42 +7,48 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath,lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id','title','artist_id','year','duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath,lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page']=='NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'],unit='ms')
+    t['hour'],t['day'],t['week_of_year'],t['month'],t['year'],t['weekday'] = \
+    t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data =  (list(zip(t , t['hour'],t['day'],t['week_of_year'],t['month'],t['year'],t['weekday'])))
+    column_labels = ('start_time', 'hour','day','week','month','year','weekday')
+    time_df = pd.DataFrame(time_data,columns=column_labels)
+    
+    time_df['start_time']= pd.to_datetime(time_df['start_time'])
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df =  df[['userId','firstName','lastName','gender','level']]
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
-
+     
+    df['ts']= pd.to_datetime(df['ts'],unit='ms')
+    
     # insert songplay records
     for index, row in df.iterrows():
         
@@ -56,7 +62,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data =  (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
